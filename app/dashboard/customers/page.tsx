8f8ec8749916785bf4d88935acd524bd9bd0594c
tian <122814453@qq.com>
2024-08-08 11:02:18 +0800

    auth

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,5 @@
-export default function Page() {
+export default async function Page() {
+  await new Promise(r => setTimeout(r, 1000));
   return (
     <div>
       <h1>cunstom</h1>
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
     images: {
         domains: ['avatars.githubusercontent.com'],
     },
+    experimental: {
+        ppr: 'incremental',
+    },
 };
 
 export default nextConfig;
